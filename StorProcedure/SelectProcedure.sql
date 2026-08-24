54dcdd3f42ce88257b915bf15c307b927c432322
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-08-24 09:47:16 +0530

    select the procedur

--- StorProcedure/SelectProcedure.sql
+++ StorProcedure/SelectProcedure.sql
@@ -1,12 +1,29 @@
-USE [OperatorDB]
+CREATE DATABASE OperatorDB;
 GO
-Create Procedure Select_tbl_Operator_sp
+
+USE OperatorDB;
+GO
+
+CREATE TABLE tbl_Operator
+(
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    mark INT,
+    cource VARCHAR(100)
+);
+GO
+--SELECT THE PROCIDURE :
+
+CREATE PROCEDURE Select_tbl_Operator_sp
 AS
-SELECT [id]
-       ,[name]
-       ,[mark]
-       ,[cource]
+BEGIN
+    SELECT
+        [id],
+        [name],
+        [mark],
+        [cource]
+    FROM [dbo].[tbl_Operator];
+END
+GO
 
-    FROM [dbo].[tbl_Operator]
-
-    GO   
+EXEC Select_tbl_Operator_sp;
